2c3f829a40ad41d65153c0c6cb1f66ba2851b876
David Navalho <david.precatado.navalho@viadirecta.pt>
2025-01-03 17:14:16 +0000

    Initial Commit

---
+++ ProfileThemeColourTests/ProfileThemeColourTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileThemeColourTests.swift
+//  ProfileThemeColourTests
+//
+//  Created by David Navalho on 03/01/2025.
+//
+
+import Testing
+@testable import ProfileThemeColour
+
+struct ProfileThemeColourTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
